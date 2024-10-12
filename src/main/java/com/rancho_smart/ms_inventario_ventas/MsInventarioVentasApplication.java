1976d2d5c00a8faeae3870f5cf7fbd1f72bbe3ec
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:18:16 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ src/main/java/com/rancho_smart/ms_inventario_ventas/MsInventarioVentasApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_inventario_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventarioVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInventarioVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/ms_inventario_ventas/MsInventarioVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_inventario_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventarioVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
