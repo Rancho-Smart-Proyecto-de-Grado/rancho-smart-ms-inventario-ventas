c262152c8949390a34ce0a93b134d9fc7d41b80e
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-21 03:33:09 -0500

    :wrench: chore: Configuración con Docker y Docker Compose

--- src/main/java/com/rancho_smart/ms_inventario_ventas/MsInventarioVentasApplication.java
+++ src/main/java/com/rancho_smart/ms_inventario_ventas/MsInventarioVentasApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.ms_inventario_ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsInventarioVentasApplication {
 
 	public static void main(String[] args) {
